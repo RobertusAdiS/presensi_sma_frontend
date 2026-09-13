de22a0ff64151d7676cae5db7064edba72f787eb
Adi Robertus <128441373+RobertusAdiS@users.noreply.github.com>
2026-09-13 17:14:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -81,7 +81,7 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 8080,
     proxy: {
       "/api": {
         target: "https://presensismabackend-production.up.railway.app",
